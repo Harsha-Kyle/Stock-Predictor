250c27f0a1c74f1e853183f073f11c45e9804cd9
Harsha-Kyle <153487969+Harsha-Kyle@users.noreply.github.com>
2025-06-18 23:53:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -193,5 +193,9 @@ def serve_react_app(path):
     return send_from_directory(app.static_folder, 'index.html')
 
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
